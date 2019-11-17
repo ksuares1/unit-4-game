a9fec9a21f3163716c8f48f8a323a6ec4e20e5f6
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-11-17 15:59:20 -0500

    working on a function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 
 
 // Initialize the game:
-var targetNumber= Math.floor(Math.random()*100);
-var wins=0;
-var losses=0;
+var totalSum=0;
 
 function startGame(){
+    targetNumber= [Math.floor(Math.random()*100)];
     for(var i= 0; i > startGame; i++){
-
+        wins=0;
+        losses=0;
     }
 }
 

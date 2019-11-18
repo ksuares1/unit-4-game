2122d3bbf3455b4740e113d7ccbdf8774d059d6c
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-11-18 16:54:41 -0500

    writing condtionals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,42 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 });
-    // this game generates a random number between 0 and 99
-    var targetNumber = Math.floor(Math.random() * 100);
+// this game generates a random number between 0 and 99
+var targetNumber = Math.floor(Math.random() * 100);
 
-    // Initialize the game:
-    var wins = 0;
-    var losses = 0;
-    
-    var totalScore = 0;
+// Initialize the game:
+var wins = 0;
+var losses = 0;
+
+var totalScore = 0;
+$('#totScore').text(totScore);
+// the crystals contain a value between 0 and 99
+var blue = Math.floor(Math.random() * 99) + 1;
+var amethyst = Math.floor(Math.random() * 99) + 1;
+var emerald = Math.floor(Math.random() * 99) + 1;
+var opal = Math.floor(Math.random() * 99) + 1;
+
+function win() {
+    wins = wins + 1;
+    $('#wins').text(wins);
+    resetTwo();
+    reset();
+}
+
+function lose() {
+    losses = losses + 1;
+    $('#losses').text(losses);
+    resetTwo();
+    reset();
+}
+
+//When user clicks a gem, it will collect the totalSum=totalSum+gemValue
+$('#blue').click(function () {
+    totScore = totScore + blue
     $('#totScore').text(totScore);
-    // the crystals contain a value between 0 and 99
-    var blue = Math.floor(Math.random() * 99) + 1;
-    var amethyst = Math.floor(Math.random() * 99) + 1;
-    var emerald = Math.floor(Math.random() * 99) + 1;
-    var opal = Math.floor(Math.random() * 99) + 1;
-
-     function win(){
-         wins = wins + 1;
-         $('#wins').text(wins);
-         resetTwo();
-         reset();
-     }
-
-     function lose(){
-        losses = losses + 1;
-        $('#losses').text(losses);
-        resetTwo();
+    if (totScore === targetNumber) {
+        win();
         reset();
-     }
-
-     //When user clicks a gem, it will collect the totalSum=totalSum+gemValue
-     $('#blue').click(function(){
-         totScore = totScore + blue
-         $('#totScore').text(totScore);
-         if(totScore === randNumber){
-             win();
-             reset();
-             resetTwo();
-         }
-
+        
+    }
+    else if(totalScore >randomNumber)

be9d92947141914c5dda075ea302f69aa210df7c
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-11-18 00:07:54 -0500

    updating js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // this game generates a random number between 0 and 99
+    var targetNumber = Math.floor(Math.random() * 100);
 
+    // Initialize the game:
+    var wins = 0;
+    var losses = 0;
 
-
-// Initialize the game:
-var totalSum=0;
-var randomNumber;
-var wins=0;
-var losses=0;
-
+    // the crystals contain a value between 0 and 99
+    var blue = Math.floor(Math.random() * 99) + 1;
+    var amethyst = Math.floor(Math.random() * 99) + 1;
+    var emerald = Math.floor(Math.random() * 99) + 1;
+    var opal = Math.floor(Math.random() * 99) + 1;
 // function startGame(){
-    //everytime we start a game- generate a random target number
-     var targetNumber= [Math.floor(Math.random()*100)];
-    console.log(targetNumber);
-//     for(var i= 0; i > startGame; i++){
-      
+// for(var i= 0; i > startGame; i++){
+
 //     }
 // }
 
